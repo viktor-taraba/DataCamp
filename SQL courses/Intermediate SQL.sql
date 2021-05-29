27cf77b2aba9a7ccd9cd8a4d2a9728e79ea0dab9
VT <55559682+Victor-T2001@users.noreply.github.com>
2021-05-29 20:11:36 +0300

    Update Intermediate SQL.sql

--- SQL courses/Intermediate SQL.sql
+++ SQL courses/Intermediate SQL.sql
@@ -143,5 +143,50 @@ LEFT JOIN matches AS m
 ON c.id = m.country_id
 GROUP BY country;
 
+SELECT 
+	-- Select the date, home goals, and away goals scored
+    date,
+	home_goal,
+	away_goal
+FROM  matches_2013_2014
+-- Filter for matches where total goals exceeds 3x the average
+WHERE (home_goal + away_goal) > 
+       (SELECT 3 * AVG(home_goal + away_goal)
+        FROM matches_2013_2014); 
+	
+SELECT 
+	-- Select the team long and short names
+	team_long_name,
+	team_short_name
+FROM team 
+-- Exclude all values from the subquery
+WHERE team_api_id NOT IN
+     (SELECT DISTINCT hometeam_id  FROM match);
+     
+SELECT
+	-- Select the team long and short names
+	team_long_name,
+	team_short_name
+FROM team
+-- Filter for teams with 8 or more home goals
+WHERE team_api_id IN
+	  (SELECT hometeam_id
+       FROM match
+       WHERE home_goal >= 8);
+       
+SELECT
+	-- Select country name and the count match IDs
+    c.name AS country_name,
+    COUNT(sub.id) AS matches
+FROM country AS c
+-- Inner join the subquery onto country
+-- Select the country id and match id columns
+INNER JOIN (SELECT country_id, id 
+           FROM match
+           -- Filter the subquery by matches with 10+ goals
+           WHERE (home_goal + away_goal) >= 10) AS sub
+ON c.id = sub.country_id
+GROUP BY country_name;
+
 
     
