79f7a6b1552c97992eb69659e14d117bf3d02244
VT <55559682+Victor-T2001@users.noreply.github.com>
2021-06-29 23:25:42 +0300

    Update Analyzing Business Data in SQL.sql

--- SQL courses/Analyzing Business Data in SQL.sql
+++ SQL courses/Analyzing Business Data in SQL.sql
@@ -408,4 +408,77 @@ CROSS JOIN quartiles
 WHERE revenue :: NUMERIC >= revenue_p25
   AND revenue :: NUMERIC <= revenue_p75;
   
-  
+SELECT DISTINCT
+  -- Select the order date
+  order_date,
+  -- Format the order date
+  TO_CHAR(order_date, 'FMDay DD, FMMonth YYYY') AS format_order_date
+FROM orders
+ORDER BY order_date ASC
+LIMIT 3;
+
+-- Set up the user_count_orders CTE
+WITH user_count_orders AS (
+  SELECT
+    user_id,
+    COUNT(DISTINCT order_id) AS count_orders
+  FROM orders
+  -- Only keep orders in August 2018
+  WHERE DATE_TRUNC('month', order_date) = '2018-08-01'
+  GROUP BY user_id)
+
+SELECT
+  -- Select user ID, and rank user ID by count_orders
+  user_id,
+  RANK() OVER(ORDER BY count_orders DESC) AS count_orders_rank
+FROM user_count_orders
+ORDER BY count_orders_rank ASC
+-- Limit the user IDs selected to 3
+LIMIT 3;
+
+-- Import tablefunc
+CREATE EXTENSION IF NOT EXISTS tablefunc;
+
+SELECT * FROM CROSSTAB($$
+  SELECT
+    user_id,
+    DATE_TRUNC('month', order_date) :: DATE AS delivr_month,
+    SUM(meal_price * order_quantity) :: FLOAT AS revenue
+  FROM meals
+  JOIN orders ON meals.meal_id = orders.meal_id
+ WHERE user_id IN (0, 1, 2, 3, 4)
+   AND order_date < '2018-09-01'
+ GROUP BY user_id, delivr_month
+ ORDER BY user_id, delivr_month;
+$$)
+-- Select user ID and the months from June to August 2018
+AS ct (user_id INT,
+       "2018-06-01" FLOAT,
+       "2018-07-01" FLOAT,
+       "2018-08-01" FLOAT)
+ORDER BY user_id ASC;
+
+-- Import tablefunc
+CREATE EXTENSION IF NOT EXISTS tablefunc;
+
+SELECT * FROM CROSSTAB($$
+  SELECT
+    -- Select eatery and calculate total cost
+    eatery,
+    DATE_TRUNC('month', stocking_date) :: DATE AS delivr_month,
+    SUM(meal_cost * stocked_quantity) :: FLOAT AS cost
+  FROM meals
+  JOIN stock ON meals.meal_id = stock.meal_id
+  -- Keep only the records after October 2018
+  WHERE DATE_TRUNC('month', stocking_date) > '2018-10-01'
+  GROUP BY eatery, delivr_month
+  ORDER BY eatery, delivr_month;
+$$)
+
+-- Select the eatery and November and December 2018 as columns
+AS ct (eatery TEXT,
+       "2018-11-01" FLOAT,
+       "2018-12-01" FLOAT)
+ORDER BY eatery ASC;
+
+
